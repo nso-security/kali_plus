4841565f83dd931e87bf22f378aca7f8cd81ec9a
wbmartin <w.brandon.martin@gmail.com>
2016-01-07 11:21:35 -0500

    Create 712_NmapStandardCall.sh

---
+++ 712_NmapStandardCall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+nmap -n -Pn -A -iL IPList.txt  -oA [CLIENT]-[PROJECT]-NmapScan
+
+#-n says don't resolve DNS
+#-Pn says don't depend on ping Sweep
+#-A says to enable OS and version detection, script scanning, and traceroute;
+#no need to put extension on the filename the -A will genreate them all
