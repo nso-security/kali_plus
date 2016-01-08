5a348d936fd6e5d5ad863603f9955cd31500d10b
wbmartin <w.brandon.martin@gmail.com>
2016-01-08 12:43:12 -0500

    Update 046_httpscreenshot.sh

--- 046_httpscreenshot.sh
+++ 046_httpscreenshot.sh
@@ -9,3 +9,6 @@ cd /opt/httpscreenshot
 chmod +x install-dependencies.sh
 ./install-dependencies.sh
 cd /opt
+
+#Don't forget, to run this, use /opt/httpscreenshot/masshttp as a guide, replace the path to /opt/masscan and text file with inscope IP addresses.
+
