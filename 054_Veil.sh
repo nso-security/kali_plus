646cfb5a3f53e29af15565ac46e77d9f8d4cb20e
wbmartin <w.brandon.martin@gmail.com>
2016-01-10 22:05:58 -0500

    Update 054_Veil.sh

--- 054_Veil.sh
+++ 054_Veil.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -v
 git clone https://github.com/Veil-Framework/Veil /opt/Veil 
 cd /opt/Veil/ && ./Install.sh -c
+#note, this will require user interaction
+/opt/Veil/Veil-Evasion/setup/setup.sh
