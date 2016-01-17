03c90f5db56106b40c8b50e76c770c60cad00373
wbmartin <w.brandon.martin@gmail.com>
2016-01-17 17:49:35 -0500

    Update and rename 010_update-and-vbox.sh to 010_vbox.sh

--- 010_vbox.sh
+++ 010_vbox.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -v
-#Update the repositories
-echo -e "\ndeb http://packages.linuxmint.com debian import\n" | tee -a /etc/apt/sources.list > /dev/null
-echo -e "\ndeb http://http.kali.org/kali sana main non-free contrib " | tee -a /etc/apt/sources.list > /dev/null
-echo -e "\ndeb-src http://http.kali.org/kali sana main non-free contrib " | tee -a /etc/apt/sources.list > /dev/null
 
-apt-get update
-apt-get upgrade -y
-apt-get dist-upgrade -y
 apt-get install linux-headers-$(uname -r) -y
 echo "Virtual Box > Devices > Insert Guest Additions > and then press any key\n"
 pause
