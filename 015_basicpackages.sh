2af82457f3abaade8b8070d8c9a2e79e75982fd1
wbmartin <w.brandon.martin@gmail.com>
2016-03-09 09:28:13 -0500

    Update 015_basicpackages.sh

--- 015_basicpackages.sh
+++ 015_basicpackages.sh
@@ -3,3 +3,5 @@ sudo apt-get install -y nmap
 sudo apt-get install -y traceroute
 sudo apt-get install -y libpcap-dev
 sudo apt-get install -y gpsd gpsd-clients
+#Revers Shells
+sudo apt-get install autossh ssh
