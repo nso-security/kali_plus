502e577394ac1e97b3a50a89117b6446d03e64a5
root <root@localhost.localdomain>
2016-03-09 14:41:50 -0500

    synching

--- 801_ReverseSSH.sh
+++ 801_ReverseSSH.sh
@@ -4,6 +4,6 @@ if test "$#" -ne 1; then
     echo "Must supply 1 argument - Middleman IP address"
 fi
 
-autossh -M 10984 -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 6666:localhost:22 remy@$1 -p 2222
+autossh -M 10984 -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 6666:localhost:22 kali_rvs@$1 
 
 #reference - https://raymii.org/s/tutorials/Autossh_persistent_tunnels.html
