eea794a42191364b9ae2df1156a08cbd5debc824
root <root@localhost.localdomain>
2016-03-10 13:29:24 -0500

    synching

--- 801_ReverseSSH.sh
+++ 801_ReverseSSH.sh
@@ -4,9 +4,12 @@ if test "$#" -ne 1; then
     echo "Must supply 1 argument - Middleman IP address"
 fi
 
-autossh -M 10984 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 6666:localhost:22 kali_rvs@$1 
+autossh -M 10984 -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 6666:localhost:22 kali_rvs@$1  
 
 #to make this run on startup, add the following to /etc/rc.local and make sure Certificates are in configured
-/opt/kali_plus/801_ReverseSSH.sh [Remote Listener IP]
+
+#/usr/bin/autossh -M 10984 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 6666:localhost:22 kali_rvs@[CLOUD_IP] &
+
+#/usr/bin/autossh -M 10986 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 8888:localhost:5900 kali_rvs@[CLOUD_IP] &
 
 #reference - https://raymii.org/s/tutorials/Autossh_persistent_tunnels.html
---
+++ 802_StartVnc.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if test "$#" -ne 1; then
+    echo "Must supply 1 argument - Middleman IP address"
+fi
+
+
+#Start x11 server
+/usr/bin/x11vnc -bg -forever -display :0 -rfbport 5900 -noxfixes -noxdamage -noxrecord -localhost
+
+#Reverse VNC Listener
+/usr/bin/autossh -M 10985 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 8888:localhost:5900 kali_rvs@$1 &
+
+
