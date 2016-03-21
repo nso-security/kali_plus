69575ec5bf59b01e8a133bf5579d854b53ca375b
wbmartin <w.brandon.martin@gmail.com>
2016-03-21 18:24:19 -0400

    Update 802_StartVnc.sh

--- 802_StartVnc.sh
+++ 802_StartVnc.sh
@@ -8,6 +8,6 @@ fi
 /usr/bin/x11vnc -bg -forever -display :0 -rfbport 5900 -noxfixes -noxdamage -noxrecord -localhost
 
 #Reverse VNC Listener
-/usr/bin/autossh -M 10985 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 8888:localhost:5900 kali_rvs@$1 &
+/usr/bin/autossh -M 10986 -N -f -o "PubkeyAuthentication=yes" -o "PasswordAuthentication=no" -i /root/.ssh/nopwd -R 8888:localhost:5900 kali_rvs@$1 &
 
 
