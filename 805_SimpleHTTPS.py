68a9a7f812a3b430a3a49d32acfe2a94119c258c
wbmartin <w.brandon.martin@gmail.com>
2016-04-15 14:58:06 -0400

    Update 805_SimpleHTTPS.py

--- 805_SimpleHTTPS.py
+++ 805_SimpleHTTPS.py
@@ -2,7 +2,33 @@
 #for creating the cert, Check out the kali
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
+import logging
+import cgi
 
-httpd = BaseHTTPServer.HTTPServer(('localhost', 443), SimpleHTTPServer.SimpleHTTPRequestHandler)
+
+class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        logging.warning("======= GET STARTED =======")
+        logging.warning(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+    def do_POST(self):
+        logging.warning("======= POST STARTED =======")
+        logging.warning(self.headers)
+        form = cgi.FieldStorage(
+            fp=self.rfile,
+            headers=self.headers,
+            environ={'REQUEST_METHOD':'POST',
+                     'CONTENT_TYPE':self.headers['Content-Type'],
+                     })
+        logging.warning("======= POST VALUES =======")
+        for item in form.list:
+            logging.warning(item)
+        logging.warning("\n")
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+httpd = BaseHTTPServer.HTTPServer(('localhost', 443), ServerHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='localhost.pem', server_side=True)
+
 httpd.serve_forever()
