7066750fc2566f550d017823249b319ceac34e64
wbmartin <w.brandon.martin@gmail.com>
2016-07-19 08:17:05 -0400

    Update 010_vbox.sh

--- 010_vbox.sh
+++ 010_vbox.sh
@@ -7,6 +7,7 @@ mkdir /opt/vbox; cp /media/cdrom/VBoxLinuxAdditions.run /opt/vbox
 chmod 755 /opt/vbox/VBoxLinuxAdditions.run 
 /opt/vbox/VBoxLinuxAdditions.run
 echo "Note, for new virtual box installs, you should also install the extension pack\n\n"
+echo "use this command: VBoxManage extpack install [DOWNLOADED EXTENSION PACK]"
 
 echo "Ok, press any key to restart.  While restarting Virtual Box > Devices > Shared Clipboard"
 shutdown -r now
