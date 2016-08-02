b14fa6ab6b2429866c34be5935e05065adb8ba6e
root <root@localhost.localdomain>
2016-08-02 10:43:19 -0400

    synching

---
+++ 061_EICAR.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash -v
+mkdir -p /opt/eicar
+wget --directory-prefix=/opt/eicar https://secure.eicar.org/eicar.com 
+wget --directory-prefix=/opt/eicar https://secure.eicar.org/eicar.com.txt 
+wget --directory-prefix=/opt/eicar https://secure.eicar.org/eicar_com.zip 
+wget --directory-prefix=/opt/eicar https://secure.eicar.org/eicarcom2.zip
+
