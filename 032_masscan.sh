f59856733aa761820a336f3abff510ded90b263f
root <root@localhost.localdomain>
2016-09-13 08:06:51 -0400

    synching

--- 032_masscan.sh
+++ 032_masscan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -v 
 
 #Massscan
-apt-get install git gcc make libpcap-dev
+apt-get install -y git gcc make libpcap-dev
 git clone https://github.com/wbmartin/masscan.git /opt/masscan
 cd /opt/masscan
 make 
--- 901_UpdateEverything.sh
+++ 901_UpdateEverything.sh
@@ -4,3 +4,4 @@ apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
 apt-get update --fix-missing
+apt autoremove
