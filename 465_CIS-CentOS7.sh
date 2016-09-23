b005c0090ade9b1994d503b8e8523a864228c942
wbmartin <w.brandon.martin@gmail.com>
2016-09-23 15:14:23 -0400

    Update 465_CIS-CentOS7.sh

--- 465_CIS-CentOS7.sh
+++ 465_CIS-CentOS7.sh
@@ -1,24 +1,10 @@
 #!/bin/sh
 echo "***CIS Ref: CentOS 7.0 – CIS Benchmark***"
-
-
-
-
 echo "***CIS Ref: CIS Benchmark Index***"
-echo "   Command: Audit"
-Audit
-
-
-
 
 echo "***CIS Ref: 1 Initial Setup***"
 
-
-
-
 echo "***CIS Ref: 1.1 Filesystem Configuration***"
-
-
 
 
 echo "***CIS Ref: 1.1.1.1 Ensure mounting of cramfs filesystems is disabled (Scored)***"
@@ -1744,72 +1730,200 @@ cat /etc/passwd | awk -F: '($3 == 0) { print $1 }'
 
 
 echo "***CIS Ref: 6.2.6 Ensure root PATH Integrity (Scored)***"
-
+if [ "`echo $PATH | grep :: `" != "" ]; then 
+  echo "Empty Directory in PATH (::)" 
+fi 
+if [ "`echo $PATH | grep :$`" != "" ]; then 
+  echo "Trailing : in PATH" 
+fi 
+p=`echo $PATH | sed -e 's/::/:/' -e 's/:$//' -e 's/:/ /g'` 
+set -- $p 
+while [ "$1" != "" ]; do 
+  if [ "$1" = "." ]; then 
+    echo "PATH contains ." shift continue 
+  fi 
+   if [ -d $1 ]; then dirperm=`ls -ldH $1 | cut -f1 -d" "` 
+     if [ `echo $dirperm | cut -c6 ` != "-" ]; then 
+        echo "Group Write permission set on directory $1" 
+     fi 
+  if [ `echo $dirperm | cut -c9 ` != "-" ]; then 
+     echo "Other Write permission set on directory $1" 
+  fi 
+  dirown=`ls -ldH $1 | awk '{print $3}'` 
+  if [ "$dirown" != "root" ] ; then 
+    echo $1 is not owned by root 
+  fi 
+  else 
+    echo $1 is not a directory
+  fi 
+  shift 
+done
 
 
 
 echo "***CIS Ref: 6.2.7 Ensure all users' home directories exist (Scored)***"
-
+cat /etc/passwd | awk -F: '{ print $1 " " $3 " " $6 }' | while read user uid dir; do 
+  if [ $uid -ge 1000 -a ! -d "$dir" -a $user != "nfsnobody" ]; then 
+    echo "The home directory ($dir) of user $user does not exist." 
+  fi 
+done
 
 
 
 echo "***CIS Ref: 6.2.8 Ensure users' home directories permissions are 750 or more restrictive (Scored)***"
-
+for dir in `cat /etc/passwd | egrep -v '(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin") { print $6 }'`; do 
+  dirperm=`ls -ld $dir | cut -f1 -d" "` 
+  if [ `echo $dirperm | cut -c6 ` != "-" ]; then 
+    echo "Group Write permission set on directory $dir" 
+  fi 
+  if [ `echo $dirperm | cut -c8 ` != "-" ]; then 
+    echo "Other Read permission set on directory $dir" 
+  fi 
+  if [ `echo $dirperm | cut -c9 ` != "-" ]; then 
+    echo "Other Write permission set on directory $dir" 
+  fi 
+  if [ `echo $dirperm | cut -c10 ` != "-" ]; then 
+    echo "Other Execute permission set on directory $dir" 
+  fi 
+done
 
 
 
 echo "***CIS Ref: 6.2.9 Ensure users own their home directories (Scored)***"
-
+cat /etc/passwd | awk -F: '{ print $1 " " $3 " " $6 }' | while read user uid dir; do 
+  if [ $uid -ge 1000 -a -d "$dir" -a $user != "nfsnobody" ]; then 
+    owner=$(stat -L -c "%U" "$dir") 
+    if [ "$owner" != "$user" ]; then 
+      echo "The home directory ($dir) of user $user is owned by $owner." 
+    fi 
+  fi 
+done
 
 
 
 echo "***CIS Ref: 6.2.10 Ensure users' dot files are not group or world writable (Scored)***"
-
+for dir in `cat /etc/passwd | egrep -v '(root|sync|halt|shutdown)' | awk -F: '($7 != "/sbin/nologin") { print $6 }'`; do 
+  for file in $dir/.[A-Za-z0-9]*; do 
+    if [ ! -h "$file" -a -f "$file" ]; then 
+      fileperm=`ls -ld $file | cut -f1 -d" "` 
+      if [ `echo $fileperm | cut -c6 ` != "-" ]; then 
+        echo "Group Write permission set on file $file" 
+      fi 
+      if [ `echo $fileperm | cut -c9 ` != "-" ]; then 
+        echo "Other Write permission set on file $file" 
+      fi 
+    fi 
+  done 
+done
 
 
 
 echo "***CIS Ref: 6.2.11 Ensure no users have .forward files (Scored)***"
-
+for dir in `cat /etc/passwd |\ awk -F: '{ print $6 }'`; do 
+  if [ ! -h "$dir/.forward" -a -f "$dir/.forward" ]; then 
+    echo ".forward file $dir/.forward exists" 
+  fi 
+done
 
 
 
 echo "***CIS Ref: 6.2.12 Ensure no users have .netrc files (Scored)***"
-
+for dir in `cat /etc/passwd |\ awk -F: '{ print $6 }'`; do 
+  if [ ! -h "$dir/.netrc" -a -f "$dir/.netrc" ]; then 
+    echo ".netrc file $dir/.netrc exists" 
+  fi 
+done
 
 
 
 echo "***CIS Ref: 6.2.13 Ensure users' .netrc Files are not group or world accessible (Scored)***"
-
+for dir in `cat /etc/passwd | egrep -v '(root|sync|halt|shutdown)' | awk -F: '($7 != "/sbin/nologin") { print $6 }'`; do 
+  for file in $dir/.netrc; do 
+    if [ ! -h "$file" -a -f "$file" ]; then 
+      fileperm=`ls -ld $file | cut -f1 -d" "` 
+      if [ `echo $fileperm | cut -c5 ` != "-" ]; then 
+        echo "Group Read set on $file" 
+      fi 
+      if [ `echo $fileperm | cut -c6 ` != "-" ]; then 
+        echo "Group Write set on $file" 
+      fi 
+      if [ `echo $fileperm | cut -c7 ` != "-" ]; then 
+        echo "Group Execute set on $file" 
+      fi 
+      if [ `echo $fileperm | cut -c8 ` != "-" ]; then 
+        echo "Other Read set on $file" 
+      fi 
+      if [ `echo $fileperm | cut -c9 ` != "-" ]; then 
+        echo "Other Write set on $file" 
+      fi 
+      if [ `echo $fileperm | cut -c10 ` != "-" ]; then 
+        echo "Other Execute set on $file" 
+      fi 
+    fi 
+  done 
+done
 
 
 
 echo "***CIS Ref: 6.2.14 Ensure no users have .rhosts files (Scored)***"
-
+for dir in `cat /etc/passwd | egrep -v '(root|halt|sync|shutdown)' | awk -F: '($7 != "/sbin/nologin") { print $6 }'`; do 
+  for file in $dir/.rhosts; do 
+    if [ ! -h "$file" -a -f "$file" ]; then 
+      echo ".rhosts file in $dir" 
+    fi 
+  done 
+done
 
 
 
 echo "***CIS Ref: 6.2.15 Ensure all groups in /etc/passwd exist in /etc/group (Scored)***"
-
+for i in $(cut -s -d: -f4 /etc/passwd | sort -u ); do 
+  grep -q -P "^.*?:[^:]*:$i:" /etc/group 
+  if [ $? -ne 0 ]; then 
+    echo "Group $i is referenced by /etc/passwd but does not exist in /etc/group" 
+  fi 
+done
 
 
 
 echo "***CIS Ref: 6.2.16 Ensure no duplicate UIDs exist (Scored)***"
-
+cat /etc/passwd | cut -f3 -d":" | sort -n | uniq -c | while read x ; do 
+  [ -z "${x}" ] && break set - $x 
+  if [ $1 -gt 1 ]; then 
+    users=`awk -F: '($3 == n) { print $1 }' n=$2 /etc/passwd | xargs` 
+    echo "Duplicate UID ($2): ${users}" 
+  fi 
+done
 
 
 
 echo "***CIS Ref: 6.2.17 Ensure no duplicate GIDs exist (Scored)***"
-
+cat /etc/group | cut -f3 -d":" | sort -n | uniq -c | while read x ; do 
+  [ -z "${x}" ] && break set - $x 
+  if [ $1 -gt 1 ]; then 
+    groups=`awk -F: '($3 == n) { print $1 }' n=$2 /etc/group | xargs` 
+    echo "Duplicate GID ($2): ${groups}" 
+  fi 
+done
 
 
 
 echo "***CIS Ref: 6.2.18 Ensure no duplicate user names exist (Scored)***"
-
+cat /etc/passwd | cut -f1 -d":" | sort -n | uniq -c | while read x ; do 
+  [ -z "${x}" ] && break set - $x 
+  if [ $1 -gt 1 ]; then uids=`awk -F: '($1 == n) { print $3 }' n=$2 /etc/passwd | xargs` 
+    echo "Duplicate User Name ($2): ${uids}" 
+  fi 
+done
 
 
 
 echo "***CIS Ref: 6.2.19 Ensure no duplicate group names exist (Scored)***"
-
-
-
-
+cat /etc/group | cut -f1 -d":" | sort -n | uniq -c | while read x ; do 
+  [ -z "${x}" ] && break set - $x 
+  if [ $1 -gt 1 ]; then 
+    gids=`gawk -F: '($1 == n) { print $3 }' n=$2 /etc/group | xargs` 
+    echo "Duplicate Group Name ($2): ${gids}" 
+  fi 
+done
+exit
