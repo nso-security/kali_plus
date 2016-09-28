15be6b400a17157d36bd7a0e67b78e49fc0a36e4
wbmartin <w.brandon.martin@gmail.com>
2016-09-28 14:02:17 -0400

    Create 510_DNSZoneTransfercheck.sh

---
+++ 510_DNSZoneTransfercheck.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# only one argument site.com or echo usage and die
+if [ -z "$1" ]; then
+echo "*** Sorry, you need to provide the domain name (e.g. site.com) like this $0 <domain name> "
+exit 0
+fi
+for nameseserver in $(host -t ns $1 |cut -d" " -f4);do
+  host -l $1 $nameserver |grep "has address"
+done
