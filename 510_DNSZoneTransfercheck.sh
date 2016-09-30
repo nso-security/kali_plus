58881f63955e6e651cd086366ae5012f01e2d90d
wbmartin <w.brandon.martin@gmail.com>
2016-09-30 16:54:50 -0400

    Update 510_DNSZoneTransfercheck.sh

--- 510_DNSZoneTransfercheck.sh
+++ 510_DNSZoneTransfercheck.sh
@@ -2,9 +2,10 @@
 
 # only one argument site.com or echo usage and die
 if [ -z "$1" ]; then
-echo "*** Sorry, you need to provide the domain name (e.g. site.com) like this $0 <domain name> "
-exit 0
+	echo "*** Sorry, you need to provide the domain name (e.g. site.com) like this $0 <domain name> "
+	exit 0
 fi
-for nameseserver in $(host -t ns $1 |cut -d" " -f4);do
-  host -l $1 $nameserver |grep "has address"
-done
+for nameserver in $(host -t ns $1 |cut -d" " -f4);do
+	  echo "-----------$nameserver ----------"
+	  host -l $1 $nameserver #|grep "has address"
+  done
