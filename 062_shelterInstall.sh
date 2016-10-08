75debc948ae111dedaec94fcf01096b7387a79e4
wbmartin <w.brandon.martin@gmail.com>
2016-10-08 10:52:52 -0400

    Create 062_shelterInstall.sh

---
+++ 062_shelterInstall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#mkdir /opt/shellter
+#wget https://www.shellterproject.com/Downloads/Shellter/Latest/shellter.zip -O /opt/shelter
+#unzip /opt/shellter/shellter.zip -d /opt/shellter/
+apt-get install shellter
